383c20e6e120355e7fff115649e9153264bf1c81
Nguyen Manh Dung <Dung.NM213838@sis.hust.edu.vn>
2025-05-04 10:19:14 +0700

    fc

---
+++ 1-3/gnb.c
@@ -0,0 +1,150 @@
+﻿#include <stdio.h>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+#include <stdlib.h>
+#include <stdint.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+/*
+UDP server : socket() → bind() → {recvfrom() và sendto()} → closesocket()
+10ms SFN tăng 1 đơn vị
+80ms gNB gửi MIB chứa SFN hiện tại cho UE
+*/
+
+#define SCALE  10
+
+
+SOCKET gnb_socket;
+struct sockaddr_in gnb_addr, ue_addr;
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t message_id;
+    uint16_t sfn_value;
+} MIB;
+#pragma pack(pop)
+
+typedef struct {
+    unsigned int  time;
+    uint16_t sfn;
+} SystemParameter;
+
+void increase_sfn(SystemParameter* s) {
+    s->sfn = (s->sfn +1) % 1024;
+}
+
+
+
+bool send_mib(SOCKET socket, const MIB* mib, struct sockaddr_in* ue_addr) {
+    int ret;
+    char buff[1024];
+    memcpy(buff, mib, sizeof(MIB));
+    ret = sendto(socket, buff, sizeof(MIB), 0, (struct sockaddr*)ue_addr, sizeof(*ue_addr));
+    if (ret == SOCKET_ERROR) {
+        printf("Error cant send data\n");
+        return false;
+    }
+   // printf("Send data sucessfull\n");
+    return true;
+}
+
+void initConnection() {
+    // khoi tao winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed\n");
+        return;
+    }
+    // khoi tao socket
+    gnb_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (gnb_socket == INVALID_SOCKET) {
+        printf("Cannot create socket\n");
+        WSACleanup();
+        return;
+    }
+    // bind add 2 socket
+    gnb_addr.sin_family = AF_INET;
+    gnb_addr.sin_port = htons(5500);
+    gnb_addr.sin_addr.s_addr = INADDR_ANY;// chấp nhận kết nối từ mọi IP
+    //inet_pton(AF_INET, "127.0.0.1", &gnb_addr.sin_addr);
+
+    if (bind(gnb_socket, (struct sockaddr*)&gnb_addr, sizeof(gnb_addr)) == SOCKET_ERROR) {
+        printf("Bind failed: %d\n", WSAGetLastError());
+        closesocket(gnb_socket);
+        WSACleanup();
+        return;
+    }
+    printf("UDP gNB start...\n");
+
+    // nhận dữ liệu & phản hồi
+    char buff[1024];
+    int ue_addr_len = sizeof(ue_addr);
+  //  while (1) {
+        memset(buff, 0, sizeof(buff));
+        int r = recvfrom(gnb_socket, buff, sizeof(buff), 0, (struct sockaddr*)&ue_addr, &ue_addr_len);
+        if (r == SOCKET_ERROR) {
+            printf("recv faile\n");
+            //break;
+            return;
+        }
+
+        buff[r] = '\0';
+        printf("Received from ue [%s:%d]: %s\n",
+            inet_ntoa(ue_addr.sin_addr), ntohs(ue_addr.sin_port), buff);
+        // gửi phản hồi
+        const char* reply = "hello from server !";
+        sendto(gnb_socket, reply, strlen(reply), 0, (struct sockaddr*)&ue_addr, sizeof(ue_addr));
+
+        printf("done init\n");
+    //}
+}
+
+
+
+int main() {
+    SystemParameter sp = { 0 };
+    sp.time = 0;
+    sp.sfn = 567;
+
+    MIB mib;
+    mib.message_id = 123;
+    mib.sfn_value = sp.sfn;
+
+    initConnection();
+    //send_mib(gnb_socket, &mib, &ue_addr);
+
+    /* 
+    giả sử hệ thống sau khi sleep 10ms * SCALE(để cho dễ quan sát log nên tăng thời gian 10ms lên gấp (SCALE) lần)
+    thì sẽ tăng biến đếm sp.time lên 10 (ms) 
+    */
+    int sleep_interval_after_scale = 10 * SCALE;
+    while (1) {
+        Sleep(sleep_interval_after_scale);
+        sp.time += 10;
+        if (sp.time % 10 == 0) {
+            increase_sfn(&sp);
+        }
+        printf("SFN now = %d ---", sp.sfn);
+        
+
+        if (sp.time % 80 == 0) {
+            mib.message_id++;
+            mib.sfn_value = sp.sfn;
+            send_mib(gnb_socket, &mib, &ue_addr);
+            printf("send MIB - M_ID = %d, SFN = %d", mib.message_id, mib.sfn_value);
+        }
+
+
+        printf("\n");
+    }
+
+
+
+    closesocket(gnb_socket);
+    WSACleanup();
+    return 0;
+}
---
+++ 1-3/ue.c
@@ -0,0 +1,177 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define SCALE  10
+#pragma comment(lib, "Ws2_32.lib")
+
+/*
+UDP client: socket() → {sendto() và recvfrom()} → closesocket()
+10ms SFN tăng 1 đơn vị
+Nếu chưa đồng bộ (ASYNC): 80ms cập nhật SFN 1 lần từ gNB
+Nếu đã đồng bộ (SYNC): 800ms cập nhật SFN 1 lần từ gNB
+*/
+
+SOCKET ue_socket;
+struct sockaddr_in gnb_addr;
+
+typedef enum {
+    ASYNC,
+    SYNC
+} State;
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t message_id;
+    uint16_t sfn_value;
+} MIB;
+#pragma pack(pop)
+
+typedef struct {
+    unsigned int time;
+    uint16_t sfn;
+    State state;
+} SystemParameter;
+
+void increase_sfn(SystemParameter* s) {
+    s->sfn = (s->sfn + 1) % 1024;
+}
+void updateSFN(SystemParameter* s, MIB* mib) {
+    s->sfn = mib->sfn_value;
+    printf(" --- update SFN = %d----- ", s->sfn);
+}
+
+void update_sfn(SystemParameter* s, uint16_t gnb_sfn) {
+    if (gnb_sfn >= 0 && gnb_sfn <= 1023) {
+        s->sfn = gnb_sfn;
+        printf("Updated SFN to %d from gNB\n", s->sfn);
+    }
+    else {
+        printf("Invalid gNB SFN\n");
+    }
+}
+
+
+
+
+bool receive_mib(SOCKET socket, MIB* mib, struct sockaddr_in* gnb_addr) {
+    int ret;
+    int addr_len = sizeof(*gnb_addr);
+    char buff[1024];
+    ret = recvfrom(socket, buff, 1024, 0, (struct sockaddr*)&gnb_addr, &addr_len);
+    if (ret == SOCKET_ERROR) {
+        if (WSAGetLastError() == WSAETIMEDOUT) {
+            printf("Time out\n");
+        }
+        else {
+            printf("Error cant receive msg\n");
+        }
+        return false;
+    }
+    if (ret < sizeof(MIB)) {
+        printf("size invalid :))), size: %d bytes\n", ret);
+        return false;
+    }
+    memcpy(mib, buff, sizeof(MIB));
+
+
+    //printf("Received MIB from gNB:\n");
+    //printf("  message_id = %d\n", mib->message_id);
+    //printf("  sfn_value  = %d\n", mib->sfn_value);    
+    return true;
+}
+void initConnection() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed\n");
+        return;
+    }
+
+    ue_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (ue_socket == INVALID_SOCKET) {
+        printf("Cannot create socket\n");
+        WSACleanup();
+        return;
+    }
+    printf("UDP ue start\n");
+    // cấu hình địa chỉ của gNB
+    gnb_addr.sin_family = AF_INET;
+    gnb_addr.sin_port = htons(5500);
+    gnb_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+
+    // gửi dữ liệu tới gnb
+    int gnb_addr_len = sizeof(gnb_addr);
+
+    const char* msg = "hello from ue";
+    int sent = sendto(ue_socket, msg, strlen(msg), 0, (struct sockaddr*)&gnb_addr, sizeof(gnb_addr));
+    if (sent == SOCKET_ERROR) {
+        printf("sent fail\n");
+        closesocket(ue_socket);
+        WSACleanup();
+        return;
+    }
+    printf("sent hello from ue to gnb :)))\n");
+
+    // nhận phản hồi từ gnb
+    char buff[1024];
+    int r = recvfrom(ue_socket, buff, sizeof(buff), 0, (struct sockaddr*)&gnb_addr,  &gnb_addr_len);
+    if (r == SOCKET_ERROR) {
+        printf("recvfrom() failed. Error Code: %d\n", WSAGetLastError());
+    }
+    else {
+        buff[r] = '\0';
+        printf("Reply from gnb: %s\n", buff);
+    }
+    printf("done init\n");
+
+}
+
+
+
+int main() {
+    SystemParameter sp = { 0 };
+    sp.time = 0;
+    sp.sfn = 0;
+    sp.state = ASYNC; // Bắt đầu ở trạng thái chưa đồng bộ
+
+    MIB mib;
+
+    initConnection();
+
+   /*
+   giả sử hệ thống sau khi sleep 10ms * SCALE(để cho dễ quan sát log nên tăng thời gian 10ms lên gấp (SCALE) lần)
+   thì sẽ tăng biến đếm sp.time lên 10 (ms)
+   */
+    int sleep_interval_after_scale = 10 * SCALE;
+    while (1) {
+        Sleep(sleep_interval_after_scale);
+        sp.time += 10;
+        if (sp.time % 10 == 0) {
+            increase_sfn(&sp);
+        }
+        printf("SFN now = %d ---", sp.sfn);
+
+        if (sp.time % 80 == 0) {
+            receive_mib(ue_socket, &mib, &gnb_addr);
+            printf("receive MIB - M_ID = %d, SFN = %d", mib.message_id, mib.sfn_value);
+        }
+
+        if (sp.state == ASYNC && sp.time % 80 == 0) {
+            updateSFN(&sp, &mib);
+            sp.state = SYNC;// đặt UE ở trạng thái đã đồng bộ 
+        }
+        if(sp.state ==SYNC  && sp.time %800 ==0) updateSFN(&sp, &mib);
+
+        printf("\n");
+    }
+
+
+    closesocket(ue_socket);
+    WSACleanup();
+    return 0;
+}
